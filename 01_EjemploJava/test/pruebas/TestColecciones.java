04fe2c397069f999fe70ff51e4c16e92e9df3268
Laura Garcia <lauragarbri@gmail.com>
2019-09-19 17:45:28 +0200

    Colecciones

--- 01_EjemploJava/test/pruebas/TestColecciones.java
+++ 01_EjemploJava/test/pruebas/TestColecciones.java
@@ -10,8 +10,12 @@ import poo.CocheRally;
 import poo.TipoCarburante;
 import poo.TipoColor;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import poo.Pelota;
 
 /**
  *
@@ -19,7 +23,7 @@ import static org.junit.Assert.*;
  */
 public class TestColecciones {
 
-    @Test
+    //@Test
     public void array() {
 
         //Inicializamos array de coches
@@ -75,6 +79,60 @@ public class TestColecciones {
     //Es exactamente lo mismo que el for each anterior pero con nombre de funcion
     public static void mostrarCoche(Coche coche){
         System.out.println(" ->" + coche.toString());
+    }
+    
+    //@Test
+    public void sets(){
+        //de la interfaz set, no se pueden repetir elementos
+        HashSet<Pelota> pelotas = new HashSet<>();//No se encuentran ordenados
+        Pelota pelota1 = new Pelota();
+        Pelota pelota2 = new Pelota();
+        Pelota pelota3 = new Pelota();
+        
+        pelotas.add(pelota1);
+        pelotas.add(pelota2);
+        pelotas.add(pelota3);
+        
+        //Intentamos añadir otra vez las mismas pelotas pero no se puede
+        pelotas.add(pelota3);
+        pelotas.add(pelota2);
+        
+        System.out.println("Set Pelotas: " + pelotas);
+        assertEquals(pelotas.size(), 3);
+        
+        //En hashSet solo podemos recorrer
+        for(Pelota has: pelotas){
+            System.out.println(has);
+        }
+    }
+    
+    @Test
+    public void maps(){
+        //los hashmap no se pueden repetir, se sobreescriben ("machacan" el valor anterior)
+        HashMap<String, Boolean> coloresCalidos = new HashMap<>();
+        coloresCalidos.put("Blanco", false);
+        coloresCalidos.put("Azul", true);
+        coloresCalidos.put("Rojo", false);
+        coloresCalidos.put("verde", false);
+        coloresCalidos.put("naranja", true);
+        
+        if(coloresCalidos.get("Rojo")){
+            System.out.println("Rojo es calido");
+        }
+        if(coloresCalidos.get("Azul")){
+            System.out.println("Azul es calido");
+        }
+        
+        System.out.println("Verde es " + coloresCalidos.get("verde"));
+        
+        //Recorrer un hashmap
+        for(Map.Entry<String, Boolean> par: coloresCalidos.entrySet()){
+            System.out.println("Clave: " + par.getKey());
+            System.out.println(" Valor: " + par.getValue());
+        }
+        
+        assertEquals(coloresCalidos.get("Azul"), true);
+        assertEquals(coloresCalidos.size(),5);
     }
     
 }
