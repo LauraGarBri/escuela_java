bf1ac00178d8f7267bc67d7e3df9c40e417a7f63
Roberto C <3149775+Trewqa@users.noreply.github.com>
2019-09-25 09:47:08 +0200

    Hilo secuencial y test.

---
+++ 01_EjemploJava/test/pruebas/TestPracticaHilos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebas;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author alumno
+ */
+public class TestPracticaHilos {
+    
+    public TestPracticaHilos() {
+    }
+}
