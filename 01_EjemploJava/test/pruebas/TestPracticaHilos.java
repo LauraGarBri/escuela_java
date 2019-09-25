b10b5dc4c9f07213e45bb4e73363bb706707be56
Laura Garcia <lauragarbri@gmail.com>
2019-09-25 11:54:46 +0200

    Ultima version

--- 01_EjemploJava/test/pruebas/TestPracticaHilos.java
+++ 01_EjemploJava/test/pruebas/TestPracticaHilos.java
@@ -1,10 +1,52 @@
 package pruebas;
 
+import StringRober.HiloCrearFichero;
+import StringRober.IQuitarEspacio;
+import StringRober.StringAux;
+import StringRober.StringAux2;
+import StringRober.StringAux3;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPracticaHilos {
     
-    public TestPracticaHilos() {
+    public TestPracticaHilos() { 
+    }
+    
+    @Test
+    public void TestPractica(){
+        
+        long start = new Date().getTime();
+        
+        String ruta = "C:\\Users\\Formacion\\Desktop\\ESCUELA_JAVA\\escuela_java.git\\01_EjemploJava\\texto.txt";
+        
+        HiloCrearFichero hilo1 = new HiloCrearFichero(1,new StringAux(),TestPracticaHilos::hiloTerminado, ruta);
+        HiloCrearFichero hilo2 = new HiloCrearFichero(2,new StringAux2(),TestPracticaHilos::hiloTerminado, ruta);
+        HiloCrearFichero hilo3 = new HiloCrearFichero(3,new StringAux3(),TestPracticaHilos::hiloTerminado, ruta);
+        
+  //      hilo1.leerFicheroEjem(ruta);
+        
+        try {
+            hilo1.start();
+            hilo1.join();
+            hilo2.start();
+            hilo2.join();
+            hilo3.start();
+            hilo3.join();
+            
+            long end = new Date().getTime();
+            //System.out.println("Tiempo total " + (end - start));
+        } catch (InterruptedException ex) {
+            Logger.getLogger(TestPracticaHilos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static Float[] hiloTerminado(Float[] param){
+        System.out.println("Callback" + param[0] + "Ha tardado: " + param[1] );
+        
+        return new Float[]{};
     }
 }
